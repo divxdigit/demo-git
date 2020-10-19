309b592d1849d75d0acdc9ccc04cd8b06952f1b3
Eve Ravignot <divxdigit@hotmail.com>
2020-10-19 19:24:50 +0200

    init project bericht

---
+++ src/main/java/be/thomasmore/graduaten/demogit/DemoGitApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/graduaten/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
